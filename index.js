e1c3d262a917f2b7e4cc388d5148c98c275e6b81
unknown <singersman@naver.com>
2021-07-28 16:19:29 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 5000; // 포트 지정
 
 const bodyParser = require("body-parser"); // body 부분에 날아오는 데이터를 받기 위해서 필요(User.js에서 설정한 key, value)
 const { User } = require("./models/User"); // User.js 형식 받아옴
+
+const config = require("./config/key");
 //application/x-www-form-urlencoded, express 최신버전에서는 npm 으로 깔지않아도 기본적으로 지원되므로 줄이 생김
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
@@ -11,19 +13,16 @@ app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://root:root123@boilerplate.hbgju.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connect...")) // 성공했을시에 콘솔에 출력
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!")); // 루트 디렉토리에서 문자열을 출력한다
+app.get("/", (req, res) => res.send("Hello World! 새해 복 많이 받으세요")); // 루트 디렉토리에서 문자열을 출력한다
 
 app.post("/register", (req, res) => {
   // 회원가입 할때 필요한 정보들을 clien에서 가져오면
