e281f76e96e43f8bc4e6a1099b12d9932eb42533
unknown <singersman@naver.com>
2021-07-29 17:34:17 +0900

    수정!

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express"); // Express 모듈을 가지고 온다
 const app = express(); // 함수를 이용해서 새로운 Express App 을 만든다
-const port = 5000; // 포트 지정
 const bodyParser = require("body-parser"); // body 부분에 날아오는 데이터를 받기 위해서 필요(User.js에서 설정한 key, value)
-const { User } = require("./models/User"); // User.js 형식 받아옴
-const config = require("./config/key");
 const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+const { User } = require("./models/User"); // User.js 형식 받아옴
 
 //application/x-www-form-urlencoded, express 최신버전에서는 npm 으로 깔지않아도 기본적으로 지원되므로 줄이 생김
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //application/json
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cookieParser());
 
 const mongoose = require("mongoose");
@@ -20,7 +20,7 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("MongoDB Connect...")) // 성공했을시에 콘솔에 출력
+  .then(() => console.log("MongoDB Connected...")) // 성공했을시에 콘솔에 출력
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World! 새해 복 많이 받으세요")); // 루트 디렉토리에서 문자열을 출력한다
@@ -28,13 +28,11 @@ app.get("/", (req, res) => res.send("Hello World! 새해 복 많이 받으세요
 app.post("/register", (req, res) => {
   // 회원가입 할때 필요한 정보들을 clien에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
-  const user = new User(req.bdoy);
+  const user = new User(req.body);
 
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
-    console.log(req.body);
     return res.status(200).json({
-      //status(200) 성공했을 때를 의미
       success: true,
     });
   });
@@ -69,5 +67,5 @@ app.post("/login", (req, res) => {
 });
 
 // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는지 확인
-
+const port = 5000; // 포트 지정
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
