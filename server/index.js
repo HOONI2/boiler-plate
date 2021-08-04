a3b2f3af1bbc55634d47a5730a568ed9bd71fe06
unknown <singersman@naver.com>
2021-08-04 11:09:34 +0900

    redux

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser"); // body 부분에 날아오는 데이
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const { User } = require("./models/User"); // User.js 형식 받아옴
+const { auth } = require("./middleware/auth");
 
 //application/x-www-form-urlencoded, express 최신버전에서는 npm 으로 깔지않아도 기본적으로 지원되므로 줄이 생김
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,7 +26,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World! 새해 복 많이 받으세요")); // 루트 디렉토리에서 문자열을 출력한다
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원가입 할때 필요한 정보들을 clien에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
   const user = new User(req.body);
@@ -38,7 +39,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -62,6 +63,29 @@ app.post("/login", (req, res) => {
           .status(200)
           .json({ loginSuccess: true, userId: user._id });
       });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
     });
   });
 });
